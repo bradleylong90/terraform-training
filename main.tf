3b34f2cd3fe394d684c8eb84f63d9f89296c10e2
Bradley Long <120462546+bradleylong90@users.noreply.github.com>
2025-01-28 15:48:26 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket         = "{jenkins-state-bl }"   
+    bucket         = "{jenkins-state-bl}"   
     key            = "terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
